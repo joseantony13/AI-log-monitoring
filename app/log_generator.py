709997b4bdf3af19b87a822e59830e721e9a77ae
joseantony13 <162410440+joseantony13@users.noreply.github.com>
2026-01-28 15:43:01 +0530

    Update log_generator.py

--- app/log_generator.py
+++ app/log_generator.py
@@ -1,1 +1,14 @@
+import random
+import time
+
+levels = ["INFO", "WARNING", "ERROR"]
+
+while True:
+    level = random.choices(levels, weights=[70, 20, 10])[0]
+    log = f"{level} - Response time: {random.randint(50, 500)}ms"
+
+    with open("app.log", "a") as f:
+        f.write(log + "\n")
+
+    time.sleep(1)
 
