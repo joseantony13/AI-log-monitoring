f0eb09f02f666bad9ae2d73a7884c3fc8e02f336
joseantony13 <162410440+joseantony13@users.noreply.github.com>
2026-01-28 15:44:14 +0530

    Update detector.py

--- app/detector.py
+++ app/detector.py
@@ -1,1 +1,10 @@
+import joblib
+import pandas as pd
+
+model = joblib.load("model.pkl")
+
+def detect(response_time):
+    df = pd.DataFrame([[response_time]], columns=["response_time"])
+    prediction = model.predict(df)
+    return prediction[0] == -1
 
