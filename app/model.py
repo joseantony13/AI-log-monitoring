02967c43955077cec88333dda3b9fae206f218e2
joseantony13 <162410440+joseantony13@users.noreply.github.com>
2026-01-28 15:43:39 +0530

    Update model.py

--- app/model.py
+++ app/model.py
@@ -1,1 +1,18 @@
+import pandas as pd
+from sklearn.ensemble import IsolationForest
+import joblib
+
+data = []
+
+with open("app.log") as f:
+    for line in f:
+        response_time = int(line.split("Response time:")[1].replace("ms", ""))
+        data.append(response_time)
+
+df = pd.DataFrame(data, columns=["response_time"])
+
+model = IsolationForest(contamination=0.1)
+model.fit(df)
+
+joblib.dump(model, "model.pkl")
 
